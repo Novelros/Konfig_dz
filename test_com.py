b12bb173f8189cdbdd2711e53ada49c3e0e2196f
Novelrom <roma.shatalov.15@mail.ru>
2024-11-23 11:26:00 +0300

    ready test_com

--- test_com.py
+++ test_com.py
@@ -6,7 +6,7 @@ class TestShellEmulator(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         # Создаем виртуальную файловую систему для тестов
-        cls.fs_archive = "test_fs.tar"
+        cls.fs_archive = "virtual_fs_archive.tar"
         cls.log = "log.file.xml"
         cls.start_script = "test_script.txt"
 
@@ -25,65 +25,63 @@ class TestShellEmulator(unittest.TestCase):
     def test_ls_testing(self):
         self.shell.cd("virtual_fs_archive")
         result = self.shell.ls()
-        self.assertIn("testdir", result)
-        self.assertIn("testdir2", result)
+        self.assertIn("ne_tochka", result)
 
     def test_ls_testdir(self):
-        self.shell.cd("testing/testdir")
+        self.shell.cd("virtual_fs_archive/ne_tochka/konfig_dz")
         result = self.shell.ls()
-        self.assertIn("testfile1.txt", result)
-        self.assertIn("testfile2.txt", result)
-        self.assertIn("testfile3.txt", result)
+        self.assertIn("start.py", result)
+        self.assertIn("tar1.py", result)
+        self.assertIn("commands.txt", result)
 
     # Тесты для команды cd
     def test_cd_to_existing_dir(self):
-        result = self.shell.cd("testing/testdir")
-        self.assertEqual(self.shell.current_dir, "testing/testdir")
+        result = self.shell.cd("virtual_fs_archive/ne_tochka/konfig_dz")
+        self.assertEqual(self.shell.current_dir, "virtual_fs_archive/ne_tochka/konfig_dz")
         self.assertEqual(result, "")
 
     def test_cd_invalid_dir(self):
-        result = self.shell.cd("nonexistent")
+        result = self.shell.cd("ne_tochka/vevn")
         self.assertIn("Directory not found", result)
 
     def test_cd_back_to_parent(self):
-        self.shell.cd("testing/testdir")
+        self.shell.cd("virtual_fs_archive/ne_tochka/konfig_dz")
         self.shell.cd("..")
-        self.assertEqual(self.shell.current_dir, "testing")
+        self.assertEqual(self.shell.current_dir, "virtual_fs_archive/ne_tochka")
 
     # Тесты для команды touch
     def test_touch_create_new_file(self):
-        self.shell.cd("testing/testdir")
+        self.shell.cd("virtual_fs_archive/ne_tochka/konfig_dz")
         result = self.shell.touch("newfile.txt")
         self.assertEqual("Файл 'newfile.txt' создан", result)
         index = str(self.shell.virtual_files).find(":")
-        self.assertEqual("testing/testdir/newfile.txt", str(self.shell.virtual_files)[2:index-1])
+        self.assertEqual("virtual_fs_archive/ne_tochka/konfig_dz/newfile.txt", str(self.shell.virtual_files)[2:index-1])
 
     def test_touch_update_existing_file(self):
-        self.shell.cd("testing/testdir2")
-        result = self.shell.touch("testfile21.txt")
-        self.assertIn("Метка времени для 'testfile21.txt' обновлена.", result)
+        self.shell.cd("virtual_fs_archive/ne_tochka/konfig_dz")
+        result = self.shell.touch("commands.txt")
+        self.assertIn("Метка времени для 'commands.txt' обновлена.", result)
 
     def test_touch_nonexistent_path(self):
-        self.shell.cd("nonexistent")
+        self.shell.cd("ne_tochka/vevn")
         result = self.shell.touch("newfile.txt")
         self.assertIn("Файл 'newfile.txt' создан", result)
 
     # Тесты для команды tac
     def test_cat_existing_file(self):
-        self.shell.cd("testing/testdir2")
-        result = self.shell.cat("testfile22.txt")
-        self.assertEqual(result, "txt.22eliftset si sihT")
+        self.shell.cd("virtual_fs_archive/ne_tochka/konfig_dz")
+        result = self.shell.cat("commands.txt")
+        self.assertEqual(result, "")
 
     def test_cat_empty_file(self):
-        self.shell.cd("testing/testdir2")
-        self.shell.touch("emptyfile.txt")
-        result = self.shell.cat("emptyfile.txt")
-        self.assertEqual(result, "Виртуальный файл 'emptyfile.txt' пуст.")
+        self.shell.cd("virtual_fs_archive/ne_tochka/konfig_dz")
+        result = self.shell.cat("session_log.xml")
+        self.assertEqual(result, "<session_log><action>ls</action><result>file1 file2 file3</result></session_log>")
 
     def test_cat_nonexistent_file(self):
-        self.shell.cd("testing/testdir2")
+        self.shell.cd("virtual_fs_archive/ne_tochka/konfig_dz")
         result = self.shell.cat("nonexistent.txt")
-        self.assertEqual("Файл testing/testdir2/nonexistent.txt не найден в архиве", result)
+        self.assertEqual("Файл virtual_fs_archive/ne_tochka/konfig_dz/nonexistent.txt не найден в архиве", result)
 
     # Тесты для команды exit
     def test_exit(self):
